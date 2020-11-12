5d1b4294523699f275b7c833a868115dc0deafc1
it-Hooligan <57491204+it-Hooligan@users.noreply.github.com>
2020-11-12 21:40:47 +0200

    update

--- js/homevorkscript.js
+++ js/homevorkscript.js
@@ -12,16 +12,38 @@ Eсть входные данные:
 Результирующие переменные вывести через console.log с подписанием того что выводится
 console.log('Parameter:', variable); */
 
-var width = a = 10,
+
+var width = 10,
+    height = 20,
+    length = 40;
+var cubicVolume = width * height * length,
+    sideArea = height * length;
+
+console.log('Кубический обьем:', cubicVolume, 'см.куб');
+console.log('Площадь стороны:', sideArea, 'см.кв');
+
+var diameter = 15,
+    circleArea = Math.PI / 4 * (diameter * diameter);
+
+console.log('Площадь стороны:', circleArea, 'см.кв');
+
+/* P.S В начале сделал как ниже, все работало 
+но какое то сообщение выдало для переменных a,b,c - что возможна 
+утечка данных, но зато более математически смотрелось ))) 
+*/
+
+/*var width = a = 10,
     height = b = 20,
     length = c = 40;
 var cubicVolume = a * b * c,
     sideArea = b * c;
 
-console.log('Кубический обьем:', cubicVolume, 'см.куб'),
+console.log('Кубический обьем:', cubicVolume, 'см.куб');
     console.log('Площадь стороны:', sideArea, 'см.кв');
 
 var diameter = d = 15,
     circleArea = Math.PI / 4 * (d * d);
 
 console.log('Площадь стороны:', circleArea, 'см.кв');
+
+*/
