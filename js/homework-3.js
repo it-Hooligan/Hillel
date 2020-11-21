6967e250f89a4fa55f92f167af8ce8c3fad5be4b
it-Hooligan <57491204+it-Hooligan@users.noreply.github.com>
2020-11-21 19:11:16 +0200

    home work task 1 final version

--- js/homework-3.js
+++ js/homework-3.js
@@ -8,109 +8,18 @@
 Так же есть способ укоротить количество комбинаций в 2 раза, кто додумается тот молодец))
 */
 
-//TODO: Кратные числа
-/* Пользователь должен ввести 3 числа.
-1 число указывает от какого числа искать.
-2 число указывает до какого числа искать.
-По третьему числу найти все числа в заданном диапазоне которые являются кратными числами к 3му числу которое ввел пользователь.
-Кратность это когда число делится на другое без остатка.
-8 % 2 === 0 получается 8 кратное 2
-8 % 3 !== 0 получается 8 не кратное 2
-
-Вывести все кратные числа.
-*/
-
-
-
-// const userValue_1 = prompt('Введите первое число');
-// const userValue_2 = prompt('Введите второе число');
-// let Value_1 = parseInt(userValue_1);
-// let Value_2 = parseInt(userValue_2);
-// for (var i = Value_1; i <= Value_2; i++) {
-//     var numberPrime = Value_2 % i !=0;
-
-//     console.log(numberPrime);
-
-
-
-
-
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const userValue_1 = prompt('Введите первое число');
-// const userValue_2 = prompt('Введите второе число');
-// let Value_1 = parseInt(userValue_1);
-// let Value_2 = parseInt(userValue_2);
-// for (Value_1; Value_1 <= Value_2; Value_1++) {
-//     var numberPrime = Value_1 / Value_2--;
-
-//     console.log(numberPrime);
-
-
-
-
-
-// }
-
-
-
-
-
-
-
-
-
 
 
 const userValue_1 = prompt('Введите первое число');
 const userValue_2 = prompt('Введите второе число');
 let n_1 = parseInt(userValue_1);
 let n_2 = parseInt(userValue_2);
-
-for (let i = n_1; i <= n_2; i++) { // идут числа в диапазоне от и до  заданого пользователем !
-    var numberPrime = i;
-    for (let j = 2; j <= i; j++) { // идет проверка на деление каждого числа  на делитель от 2 до самого значения этого числа .
-        if (i % j == 0) {
-            break;
-
+lot:
+    for (let i = n_1; i <= n_2; i++) { // идут числа в диапазоне от и до  заданого пользователем !
+        var numberPrime = i;
+        for (let j = 2; j < i; j++) { // делим с проверкой кажое число верхнего цикла на делитель j .
+            if (i % j == 0) continue lot;
         }
 
-        console.log(` ${numberPrime} `);
-
+        console.log(`Простое число ${numberPrime} `);
     }
-    //??  
-}
-
-
-
-
-// for (var i = Value_1; i <= Value_2; i++) {
-
-
-//     console.log(Value_Prime);
-
-
-//     // console.log(i);
-
-//     // console.log(i);
-//     // console.log(numberPrime);
-
-
-//     for (var j = 1; j < i; j++); {
-//         // console.log(j);
-
-//     }
-// console.log(i);
-// var numberPrime = j / i;
