c4d5707b321d100135f9fdf66b6ed656ed22725a
it-Hooligan <57491204+it-Hooligan@users.noreply.github.com>
2020-12-06 21:45:39 +0200

    dolorem

--- js/homework-7.js
+++ js/homework-7.js
@@ -1,12 +1,20 @@
 //TODO
 
+// hasSpam(texts, spamWords);
+
+// function hasSpam(text, spamWords) {
+//     const textUpp = text => text.toUpperCase();
+//     console.log(textUpp);
+
+//     const spamWordsUpp = spamWords.map(str => str.toUpperCase());
+//     console.log(spamWordsUpp);
 
 
 
-function hasSpam(text, spamWords) {
-    // Функция должна проврять есть ли в тексте запрещенные слова, возвращать true или false в зависимости от наличия этих слов
-    // Регистр текста влиять не влияет на результат, слово как из больших букв так и из маленьких если есть в массиве spamWords будет означать что текст содержит спам
-}
+
+// Функция должна проврять есть ли в тексте запрещенные слова, возвращать true или false в зависимости от наличия этих слов
+// Регистр текста влиять не влияет на результат, слово как из больших букв так и из маленьких если есть в массиве spamWords будет означать что текст содержит спам
+// }
 
 function replaceSpam(text, spamWords, replaceWith) {
     // Данная функция должна находить запрещенные слова в тексте, и удалять его при помощи текста переданного в агрументе replaceWith
@@ -25,18 +33,61 @@ function replaceSpam(text, spamWords, replaceWith) {
 
 // Дальнейший код нужен исключительно для проверки результата работы функций
 // Менять его не нужно
+
+
+
+
+
+
+
+
+
+
 const texts = [
     'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam, voluptatibus maiores recusandae, perferendis tempore odit et aut dolore, voluptate porro expedita ipsa tenetur reiciendis illum delectus doloremque error magnam similique.',
     'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod mollitia voluptatibus, ipsam earum ipsum quasi doloribus iste alias veniam, quae velit qui voluptatum cupiditate, provident aspernatur, eos dolorem eius! Cupiditate.',
     'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel autem similique a porro itaque voluptate fuga quo, commodi laboriosam, quis expedita sapiente amet, placeat quibusdam provident vero ex. Eveniet, iusto?',
+    'kaka mota',
 ];
 
 const spamWords = [
     'dolorem',
-    'commodi',
+    'dolorem',
     'adipisicing',
     'doloribus',
 ];
+
+
+function hasSpam(text, spamWords) {
+    const textUpp = text.map(text => text.toUpperCase());
+    console.log(textUpp);
+
+    const spamWordsUpp = spamWords.map(str => str.toUpperCase());
+    console.log(spamWordsUpp);
+
+    const noSpam = textUpp.filter(value => !value.includes('DOLOREM'));
+
+    console.log(noSpam);
+}
+// });
+hasSpam(texts, spamWords);
+
+
+
+
+
+// function dirR(xd) {
+//     const spamWordsUpp = xd.map(str => str.toUpperCase());
+//     console.log(spamWordsUpp);
+// }
+// dirR(spamWords);
+
+
+// function hasSpam(spamWords) {
+//     let spamWordsUpp = spamWords.map(str.spamWords => );
+//     console.log(spamWordsUpp);
+
+// // }
 
 texts.forEach(text => {
     const isTextContainsSpam = hasSpam(text, spamWords);
